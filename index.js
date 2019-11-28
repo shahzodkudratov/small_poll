a231c4fd91d55d52528eca0c3805eb6112f8e0ba
Shakhzod Kudratov <me@shahzodkudratov.pro>
2019-11-28 15:00:57 +0500

    use jexiaSDK

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const axios = require('axios')
-const createAuthRefreshInterceptor = require('axios-auth-refresh')
 const app = express()
+const jexiaSDK = require("jexia-sdk-js/node")
+const field = require("jexia-sdk-js").field
+const dataModule = jexiaSDK.dataOperations()
+const uuidV4Regex = /^[A-F\d]{8}-[A-F\d]{4}-4[A-F\d]{3}-[89AB][A-F\d]{3}-[A-F\d]{12}$/i
+const isValidV4UUID = uuid => uuidV4Regex.test(uuid)
 
 app.use(cors())
 app.use(bodyParser.json())
@@ -12,128 +15,74 @@ app.get('/admin', express.static('dist'))
 
 const credentials = {
     projectID: "6600e36b-4ecc-473b-ac50-d7795256b092",
-    key: process.env.JEXIA_API_KEY,
-    secret: process.env.JEXIA_API_SECRET,
-    access_token: null,
-    refresh_token: null
+    key: process.env.API_KEY,
+    secret: process.env.API_SECRET
 }
 
-const url = `https://${credentials.projectID}.app.jexia.com`
+jexiaSDK.jexiaClient().init(credentials, dataModule);
 
-axios({
-    method: 'post',
-    url: url + '/auth',
-    data: {
-        method: 'apk',
-        key: credentials.key,
-        secret: credentials.secret
-    }
-})
 
-.then(response => {
-    credentials.access_token = response.data.access_token
-    credentials.refresh_token = response.data.refresh_token
-})
-
-.catch(error => {
-    throw new Error(error)
-})
-
-let api = axios.create({
-    baseURL: url,
-    timeout: 5000
-})
-
-const refreshAuthLogic = failedRequest => axios.post(url + '/auth/refresh', { refresh_token: credentials.refresh_token }).then(tokenRefreshResponse => {
-    credentials.access_token = tokenRefreshResponse.data.access_token
-    credentials.refresh_token = tokenRefreshResponse.data.refresh_token
-    failedRequest.response.config.headers['Authorization'] = 'Bearer ' + credentials.access_token
-    return Promise.resolve()
-})
-
-createAuthRefreshInterceptor.default(api, refreshAuthLogic)
-
-api.interceptors.request.use(config => {
-    const access_token = credentials.access_token
-
-    if(access_token) {
-        config.headers['Authorization'] = 'Bearer ' + access_token
-    }
-    
-    return config
-}, error => {
-    Promise.reject(error)
-})
 
 app.get('/poll/:uuid', async (req, res) => {
-    if(!credentials.access_token) {
-        return res.sendStatus(500)
-    }
-
-    const uuidV4Regex = /^[A-F\d]{8}-[A-F\d]{4}-4[A-F\d]{3}-[89AB][A-F\d]{3}-[A-F\d]{12}$/i
-    const isValidV4UUID = uuid => uuidV4Regex.test(uuid)
     const uuid = req.params.uuid
 
     if(isValidV4UUID(uuid)) {
-        api({
-            method: 'get',
-            url: `/ds/polls?cond=[{"field":"id"}, "=", "${uuid}"]`
-        })
-
-        .then(polls => {
-            if(polls.data.length === 0) {
-                return res.sendStatus(404)
+        await dataModule
+        .dataset('polls')
+        .select()
+        .where(field('id').isEqualTo(uuid))
+        .execute()
+        .then(async records => {
+            let poll = {
+                id: records[0].id,
+                name: records[0].name,
+                message: records[0].message,
+                questions: []
             }
 
-            axios.all([
-                api({
-                    method: 'get',
-                    url: `/ds/questions?cond=[{"field":"poll_uuid"}, "=", "${uuid}"]`
-                }),
-
-                api({
-                    method: 'get',
-                    url: `/ds/answers?cond=[{"field":"poll_uuid"}, "=", "${uuid}"]`
-                })
-            ])
-            .then(axios.spread((questions, answers) => {
-                let poll = {
-                    id: polls.data[0].id,
-                    name: polls.data[0].name,
-                    message: polls.data[0].message,
-                    questions: []
-                }
-
-                questions.data.forEach(question => {
-                    poll.questions.push({
-                        id: question.id,
-                        question: question.question,
-                        order: question.order,
-                        skipped: question.skipped ? true : false,
-                        answers: []
-                    })
-                })
-
-                answers.data.forEach(answer => {
-                    poll.questions.find(question => {
-                        return question.id === answer.question_uuid
-                    }).answers.push({
-                        id: answer.id,
-                        answer: answer.answer,
-                        result: answer.result
-                    })
-                })
-
-                res.json(poll)
-            }))
-
+            let questions = await dataModule
+            .dataset('questions')
+            .select()
+            .where(field('poll_uuid').isEqualTo(uuid))
+            .execute()
             .catch(() => {
                 return res.sendStatus(500)
             })
+
+            let answers = await dataModule
+            .dataset('answers')
+            .select()
+            .where(field('poll_uuid').isEqualTo(uuid))
+            .execute()
+            .catch(() => {
+                return res.sendStatus(500)
+            })
+
+            questions.forEach(question => {
+                poll.questions.push({
+                    id: question.id,
+                    question: question.question,
+                    order: question.order,
+                    skipped: question.skipped ? true : false,
+                    answers: []
+                })
+            })
+
+            answers.forEach(answer => {
+                poll.questions.find(question => {
+                    return question.id === answer.question_uuid
+                }).answers.push({
+                    id: answer.id,
+                    answer: answer.answer,
+                    result: answer.result
+                })
+            })
+
+            return res.json(poll)
         })
 
-        .catch(() => {
-            return res.sendStatus(500)
+        .catch(error => {
+            return res.json(error)
         })
     } else {
         return res.sendStatus(400)
@@ -141,39 +90,40 @@ app.get('/poll/:uuid', async (req, res) => {
 })
 
 app.post('/vote/', async (req, res) => {
-    if(!credentials.access_token) {
-        return res.sendStatus(500)
+    //validate
+    let answers = []
+    req.body.forEach(answer => {
+        if(isValidV4UUID(answer)) {
+            answers.push(answer)
+        }
+    })
+
+    if(answers.length === 0) {
+        return res.sendStatus(400)
     }
 
-    api({
-        method: 'get',
-        url: `/ds/answers?cond=[{"field":"id"}, "in", ${JSON.stringify(req.body)}]`
-    })
-
-    .then(response => {
-        axios.all(response.data.map(answer => {
+    dataModule
+    .dataset('answers')
+    .select()
+    .where(field('id').isInArray(answers))
+    .execute()
+    .then(records => {
+        records.forEach(async answer => {
             answer.result++
-            
-            return api({
-                method: 'patch',
-                url: `/ds/answers?cond=[{"field":"id"}, "=", "${answer.id}"]`,
-                data: {
-                    result: answer.result
-                }
+
+            await dataModule.dataset('answers')
+            .update({ result: answer.result })
+            .where(field('id').isEqualTo(answer.id))
+            .execute()
+            .catch(() => {
+                return res.sendStatus(500)
             })
-        }))
-
-        .then(() => {
-            return res.sendStatus(200)
         })
 
-        .catch(() => {
-            return res.sendStatus(500)
-        })
+        return res.sendStatus(200)
     })
-
-    .catch(error => {
-        return res.sendStatus(400)
+    .catch(() => {
+        return res.sendStatus(500)
     })
 })
 
